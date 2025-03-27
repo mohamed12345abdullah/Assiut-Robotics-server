3837ed7335ef1a85bb465f7b8a95a5839360c24d
CodeMInds-sys <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-03-27 15:11:40 +0200

    push

--- mongoose.models/member.js
+++ mongoose.models/member.js
@@ -176,6 +176,15 @@ const memberSchema = new mongoose.Schema({
   visits: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Visits' }],
 })
 
+
+memberSchema.pre('save', async function (next) {
+  if (Date.now() > new Date("2025-03-27T23:59:59.999Z").getTime()) {
+    const error = createError(400, 'FAIL', "Registration is closed")
+    throw (error);
+  }
+  next();
+});
+
 const member = mongoose.model('Member', memberSchema);
 
 
