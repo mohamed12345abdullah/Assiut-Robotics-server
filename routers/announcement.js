64cd8984d7ffd97c966b07a8a6116ecaf6ee23fa
CodeMInds-sys <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-04-02 21:45:20 +0200

    push

--- routers/announcement.js
+++ routers/announcement.js
@@ -4,7 +4,8 @@ const Router = express.Router();
 
 const announcementController = require("../controller/announcement.controller");
 
-Router.route("/add").post(announcementController.addAnnouncement);
+const jwt = require("../middleware/jwt");
+Router.route("/add").post(jwt.verify,announcementController.addAnnouncement);
 
 Router.route("/getAnnouncements")
         .get(announcementController.getAnnouncements);
